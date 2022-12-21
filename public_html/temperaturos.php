707e553ab1856750c65faa613f177121078968d6
Tautvydas Dulskis <tautvydas@dulskis.eu>
2022-12-21 14:40:20 +0200

    Konvertavimas i Celcius

--- public_html/temperaturos.php
+++ public_html/temperaturos.php
@@ -2,6 +2,15 @@
 
 $temp = [78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73];
 
+function konvertuotiFarenheita($array)
+{
+    $farenheita = [];
+    foreach ($array as $temp) {
+        $farenheita[] = round(($temp - 32) * 5 / 9, 0);
+    }
+    return $farenheita;
+}
+
 function masyvoVidurkis($array) {
     $suma = null;
     foreach ($array as $element) {
@@ -36,6 +45,10 @@ function spausdintiGrafiskai($temp, $kiek)
 }
 
 echo "Grafikas: ";
+spausdintiGrafiskai($temp, count($temp));
+
+$temp = konvertuotiFarenheita($temp);
+echo "Grafikas (C): ";
 spausdintiGrafiskai($temp, count($temp));
 
 // 1-ma uzduotis
