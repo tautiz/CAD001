07963be0a6392f10439ea0786c51fbf650524515
Tautvydas Dulskis <tautvydas@dulskis.eu>
2022-12-21 14:31:28 +0200

    Temperaturu uzduotis

---
+++ public_html/temperaturos.php
@@ -0,0 +1,53 @@
+<?php
+
+$temp = [78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73];
+
+function masyvoVidurkis($array) {
+    $suma = null;
+    foreach ($array as $element) {
+        $suma += $element;
+    }
+
+    return $suma / count($array);
+}
+
+function spausdintiPirmusElementus($array, $kiek) {
+    for ($i = 0; $i < $kiek; $i++) {
+        echo " " . $array[$i];
+    }
+
+    echo "<br>";
+}
+
+function spausdintiGrafiskai($temp, $kiek)
+{
+    for ($i = 0; $i < $kiek; $i++) {
+        echo "<div style='
+                    height: $temp[$i]px;
+                    background-color: gray; 
+                    display: inline-block;
+                    padding: 3px;
+                    border: 1px solid red;'>
+                 $temp[$i]
+              </div>";
+    }
+
+    echo "<br>";
+}
+
+echo "Grafikas: ";
+spausdintiGrafiskai($temp, count($temp));
+
+// 1-ma uzduotis
+$vidurkis = masyvoVidurkis($temp);
+// Arba
+//$vidurkis = array_sum($temp) / count($temp);
+echo "Vidutinė temperatūra: $vidurkis";
+
+rsort($temp);
+echo "<br>Penkios auksciausios: ";
+spausdintiPirmusElementus($temp, 5);
+
+sort($temp);
+echo "Penkios zemiausios: ";
+spausdintiPirmusElementus($temp, 5);
