6e2c507275add759036fcf44143b5f89bee39009
Tautvydas Dulskis <tautvydas@dulskis.eu>
2022-12-22 11:40:11 +0200

    Darbas su JSON

--- public_html/index.php
+++ public_html/index.php
@@ -49,13 +49,60 @@ function skaiciuotiStudentuAmziausVidurki(array $studentai): float
     return $suma / $kiekis;
 }
 
+
+// ---------------------------- Studento [CRUD] ----------------------------
+function sukurtiStudenta(array $studentas): void
+{
+    $studentai = gautiStudentus(); // Gauti senus studentus
+    $studentai[] = $studentas;     // pridedam naują studentą prie senų
+    $jsonString = json_encode($studentai); // konvertuojam naują studentų masyvą į json string
+    file_put_contents('duomenys.json', $jsonString); // įrašom naują json string į failą
+}
+
+function gautiStudenta(int $id): array
+{
+    $studentai = gautiStudentus(); // Gauti senus studentus
+    $studentas = $studentai[$id];
+
+    return $studentas;
+}
+
+function atnaujintiStudenta(int $id, array $studentas): void
+{
+    $studentai = gautiStudentus(); // Gauti senus studentus
+    $studentai[$id] = $studentas;  // pakeičiam seną studentą nauju
+    $jsonString = json_encode($studentai); // konvertuojam naują studentų masyvą į json string
+    file_put_contents('duomenys.json', $jsonString); // įrašom naują json string į failą
+}
+
+function istrintiStudenta(int $id): void
+{
+    $studentai = gautiStudentus(); // Gauti senus studentus
+    unset($studentai[$id]);        // ištrinam studentą
+    $jsonString = json_encode($studentai); // konvertuojam naują studentų masyvą į json string
+    file_put_contents('duomenys.json', $jsonString); // įrašom naują json string į failą
+}
+
 // ---------------------------- Baigiu deklaracijas ----------------------------
 
-// ---------------------------- Pradedu vykdyma ----------------------------
+// ----------------------------Ppradedu isvedima ----------------------------
+$studentai = gautiStudentus();
+echo 'Pries sukurima senas vidurkis: ' . skaiciuotiStudentuAmziausVidurki($studentai);
+
+sukurtiStudenta(['vardas' => 'Linas', 'pavarde' => 'Linutis', 'amzius' => 50]);
 
 $studentai = gautiStudentus();
+echo '<br>Su nauju studentu vidurkis: ' . skaiciuotiStudentuAmziausVidurki($studentai);
 
-// ---------------------------- Baigiu vykdyma ----------------------------
+//$paskutinisStudentas = gautiStudenta(count($studentai) - 1);
 
-// ----------------------------Ppradedu isvedima ----------------------------
-echo skaiciuotiStudentuAmziausVidurki($studentai);
+atnaujintiStudenta(0, ['vardas' => 'Rokas', 'pavarde' => 'Rokutis', 'amzius' => 99]);
+
+$studentai = gautiStudentus();
+echo '<br>Naujas vidurkis po atnaujinimo: ' . skaiciuotiStudentuAmziausVidurki($studentai);
+
+$paskutinioStudentoId = count($studentai) - 1;
+istrintiStudenta($paskutinioStudentoId);
+
+$studentai = gautiStudentus();
+echo '<br>Naujas vidurkis po trynimo: ' . skaiciuotiStudentuAmziausVidurki($studentai);
