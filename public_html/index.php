462359e3ebab31136b3141d312b8e909a9c64134
Tautvydas Dulskis <tautvydas@dulskis.eu>
2022-12-28 14:11:03 +0200

    PHP Nauja uzduotis su OOP ir masyvais

--- public_html/index.php
+++ public_html/index.php
@@ -1,17 +1,32 @@
 <?php
 
-include '../src/Car.php';
+include '../src/Asmuo.php';
+include '../src/DuomanuIsvedimas.php';
 
-$bmw = new Car();
-$bmw->spalva = 'Raudonas';
-$bmw->greitis = '100 km/h';
-$bmw->vaziuoti(1.5);
-echo '<br>Rida: ' . $bmw->gautiRida();
+$asmenuDuomanys = [
+    ['vardas' => 'Jonas', 'gimimo_metai' => 1965],
+    ['vardas' => 'Petras', 'gimimo_metai' => 1970],
+    ['vardas' => 'Antanas', 'gimimo_metai' => 1980],
+    ['vardas' => 'Ona', 'gimimo_metai' => 1990],
+    ['vardas' => 'Maryte', 'gimimo_metai' => 2000],
+    ['vardas' => 'Petras', 'gimimo_metai' => 1986],
+    ['vardas' => 'Antanas', 'gimimo_metai' => 2005],
+];
 
-echo '<hr>';
-$audi = new Car();
-$audi->spalva = 'Juoda';
-$audi->greitis = '120 km/h';
-$audi->vaziuoti(2);
-echo '<br>Rida: ' . $audi->gautiRida();
+// Sukam cikla su kiekvienu įrašu iš $asnemuDuomanys masyvo
+// ir kiekvieną kartą sukuriam naują new Asmuo() objektą
+// naujai sukurtą objektą įdedam į $asmenys masyvą
+// Sukuriate Klasę DuomanuIsvedimas
+// Klasėje DuomanuIsvedimas sukuriame metodą isvestiAsmenis($asmenys)
+// Metodas isvestiAsmenis($asmenys) turi atspausdinti visus asmenis
+// BONUS: Metodas isvestiAsmenisPagalData($asmenys, $gmmd) turi atspausdinti asmenis pagal gimimo metus
+// BONUS: Metodas isvestiAsmenisLentele($asmenys) turi atspausdinti asmenis <table> HTML elemente
 
+foreach (/** Čia kažkas bus */) {
+    $asmuo = new Asmuo('Jonas', 1990);
+    /** Čia kažkas bus */
+}
+
+/** Čia kažkas bus */
+
+isvestiAsmenis($asmenys);
