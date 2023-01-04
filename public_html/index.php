7014cb32e775521c0202d79a843c86de400d908e
Tautvydas Dulskis <tautvydas@dulskis.eu>
2023-01-04 12:24:11 +0200

    PHP primityvus prisijungimas

--- public_html/index.php
+++ public_html/index.php
@@ -5,7 +5,26 @@ use Monolog\Handler\StreamHandler;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$log = new Logger('name');
-$log->pushHandler(new StreamHandler('path/to/your.log', Logger::WARNING));
-$log->warning('Foo');
-$log->error('Bar');
+$log = new Logger('Portfolios');
+$log->pushHandler(new StreamHandler('../logs/klaidos.log', Logger::WARNING));
+
+try {
+    $failoAdresas = '../src/html/pradzia.html';
+
+    $failoTurinys = file_get_contents($failoAdresas);
+
+    echo $failoTurinys;
+
+    $userName = $_POST['username'] ?? null;
+    $password = $_POST['password'] ?? null;
+
+   if ($userName === 'admin' && $password === 'slapta') {
+        echo 'Sveiki prisijunge';
+    } elseif ($userName !== null && $password !== null) {
+        echo 'Neteisingi prisijungimo duomenys';
+    }
+
+} catch (Exception $e) {
+    echo 'Oi nutiko klaida! Bandyk vėliau dar karta.';
+    $log->error($e->getMessage());
+}
